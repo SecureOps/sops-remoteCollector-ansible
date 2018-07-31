aed4e7c687c25237a0d3c05096e5f29aefcd62b9
Srinath M <smantripragada@secureops.com>
2018-07-31 15:09:59 -0400

    Adding shell script that calls CloudFormation script via aws cli

---
+++ scripts/createAWSCustomerStack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -u
+
+## LOAD required customer related vars
+SCRIPT_DIR=$( dirname ${BASH_SOURCE[0]} )
+. ${SCRIPT_DIR}/../vars.sh
+
+
+aws cloudformation create-stack --stack-name "${CUSTOMER_NAME}Stack" \
+                                --capabilities CAPABILITY_NAMED_IAM \
+                                --template-body file:///${SCRIPT_DIR}/createAWSResoucesCustomer_cf.json \
+                                --parameters ParameterKey=CustomerName,ParameterValue=${CUSTOMER_NAME} ParameterKey=CustomerS3BucketName,ParameterValue=${CUSTOMER_NAME,,}
+
+
+
+aws cloudformation describe-stacks --stack-name "${CUSTOMER_NAME}Stack"
+
