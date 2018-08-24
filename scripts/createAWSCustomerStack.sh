5799ae56a4498cfd8107698f37486e059b3a1f56
Srinath M <smantripragada@secureops.com>
2018-08-24 15:58:46 -0400

    - setting CF script as a variable

--- scripts/createAWSCustomerStack.sh
+++ scripts/createAWSCustomerStack.sh
@@ -4,12 +4,13 @@ set -u
 
 ## LOAD required customer related vars
 SCRIPT_DIR=$( dirname ${BASH_SOURCE[0]} )
+SCRIPT_DIR=$( readlink -f ${SCRIPT_DIR} )
 . ${SCRIPT_DIR}/../vars.sh
 
 
 aws cloudformation create-stack --stack-name "${CUSTOMER_NAME}Stack" \
                                 --capabilities CAPABILITY_NAMED_IAM \
-                                --template-body file:///${SCRIPT_DIR}/createAWSResoucesCustomer_cf.json \
+                                --template-body file:///${SCRIPT_DIR}/${CF_FILE} \
                                 --parameters ParameterKey=CustomerName,ParameterValue=${CUSTOMER_NAME} ParameterKey=CustomerS3BucketName,ParameterValue=${CUSTOMER_NAME,,}
 
 
