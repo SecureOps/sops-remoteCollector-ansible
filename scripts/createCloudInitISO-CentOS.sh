70422e166bdeb0a8fb6f3beea3b558343ce30fa1
Patrick Ethier <patrick@secureops.com>
2020-09-16 19:46:15 +0200

    Add the instance ID to the phone home path

--- scripts/createCloudInitISO-CentOS.sh
+++ scripts/createCloudInitISO-CentOS.sh
@@ -95,7 +95,7 @@ runcmd:
 
 
 phone_home:
- url: ${PHONE_HOME_URL}/${CUSTOMER_NAME}_data/cloud-init-report
+ url: ${PHONE_HOME_URL}/${CUSTOMER_NAME}/${INSTANCE_ID}/_data/cloud-init-report
  post: [ "${CUSTOMER_NAME}-${REMOTE_NODE_NAME}", pub_key_dsa, pub_key_rsa, pub_key_ecdsa, instance_id ]
 
 EOF
