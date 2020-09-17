4668ebaf754d4ed21ad2bb6049f9feab75717153
Patrick Ethier <patrick@secureops.com>
2020-09-17 22:13:32 +0200

    Re-arrange stacks and permissions and adding SQS response queue

--- scripts/createAWSCustomerStack.sh
+++ scripts/createAWSCustomerStack.sh
@@ -12,7 +12,7 @@ echo "Customer: ${CUSTOMER_NAME}"
 CUSTOMER_LOWER=$(echo "${CUSTOMER_NAME}" | tr '[:upper:]' '[:lower:]')
 echo "Customer Lower: ${CUSTOMER_LOWER}"
 
-# Check if the stack exists
+# Check if the customer stack exists
 CUST_STACK_CHECK=$(aws cloudformation describe-stacks --stack-name "sopsCustomer-${CUSTOMER_NAME}" || echo "Does not exist")
 if [[ $CUST_STACK_CHECK =~ "not exist" ]]; then
   aws cloudformation create-stack --stack-name "sopsCustomer-${CUSTOMER_NAME}" \
@@ -25,4 +25,22 @@ else
                                   --capabilities CAPABILITY_NAMED_IAM \
                                   --template-file ${SCRIPT_DIR}/${CF_FILE} \
                                   --parameter-overrides ParameterKey=CustomerName,ParameterValue=${CUSTOMER_NAME} ParameterKey=CustomerS3BucketName,ParameterValue=${CUSTOMER_LOWER}
+fi
+
+echo ${CF_SQS_RESP_FILE}
+if [[ ! -z "${CF_SQS_RESP_FILE}" ]]; then
+  # Check if the SQS response stack exists
+  SQS_RESPONSE_STACK_CHECK=$(aws cloudformation describe-stacks --stack-name "sopsCustomer-${CUSTOMER_NAME}-sqs-command-response" || echo "Does not exist")
+  if [[ $SQS_RESPONSE_STACK_CHECK =~ "not exist" ]]; then
+    aws cloudformation create-stack --stack-name "sopsCustomer-${CUSTOMER_NAME}-sqs-command-response" \
+                                    --capabilities CAPABILITY_NAMED_IAM \
+                                    --template-body file://${SCRIPT_DIR}/${CF_SQS_RESP_FILE} \
+                                    --parameters ParameterKey=CustomerName,ParameterValue=${CUSTOMER_NAME}
+  else
+
+    aws cloudformation deploy --stack-name "sopsCustomer-${CUSTOMER_NAME}-sqs-command-response" \
+                                    --capabilities CAPABILITY_NAMED_IAM \
+                                    --template-file ${SCRIPT_DIR}/${CF_SQS_RESP_FILE} \
+                                    --parameter-overrides ParameterKey=CustomerName,ParameterValue=${CUSTOMER_NAME}
+  fi
 fi
