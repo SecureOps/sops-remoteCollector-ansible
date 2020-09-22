3a5b1382f6e1b1ada86f2d2dd7580b6b605adb3b
Srinath M <smantripragada@secureops.com>
2020-09-22 16:30:00 -0400

    - README updates - renamed dir from cloudformation to CloudFormation (to match some scripts) - simplified the CF invocation shell scripts

--- scripts/createAWSNodeStack.sh
+++ scripts/createAWSNodeStack.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-set -e
 set -u
 
 ## LOAD required customer related vars
 SCRIPT=$( basename ${BASH_SOURCE[0]} )
 SCRIPT_DIR=$( dirname ${BASH_SOURCE[0]} )
 CF_DIR="${SCRIPT_DIR}/../CloudFormation/"
-# SCRIPT_DIR=$( readlink ${SCRIPT_DIR} )
 . ${SCRIPT_DIR}/../vars.sh
+
 echo "Script Dir: ${SCRIPT_DIR}"
+echo "CloudFormation Dir: ${CF_DIR}"
 echo "Customer: ${CUSTOMER_NAME}"
 CUSTOMER_LOWER=$(echo "${CUSTOMER_NAME}" | tr '[:upper:]' '[:lower:]')
 echo "Customer Lower: ${CUSTOMER_LOWER}"
 
-REMOTE_STACK_CHECK=$(aws cloudformation describe-stacks --stack-name "sopsCustomer-${CUSTOMER_NAME}-${REMOTE_NODE_NAME}" || echo "Does not exist")
-if [[ $REMOTE_STACK_CHECK =~ "not exist" ]]; then
-  aws cloudformation create-stack --stack-name "sopsCustomer-${CUSTOMER_NAME}-${REMOTE_NODE_NAME}" \
-                                  --capabilities CAPABILITY_NAMED_IAM \
-                                  --template-body file://${CF_DIR}/${REMOTE_NODE_CF_FILE} \
-                                  --parameters ParameterKey=RemoteNodeName,ParameterValue=${REMOTE_NODE_NAME} ParameterKey=CustomerName,ParameterValue=${CUSTOMER_NAME} ParameterKey=CustomerS3BucketName,ParameterValue=${CUSTOMER_LOWER}
-
-else
-  aws cloudformation deploy --stack-name "sopsCustomer-${CUSTOMER_NAME}-${REMOTE_NODE_NAME}" \
+## Create Node Stack
+echo
+echo "Creating Node stack 'sopsCustomer-${CUSTOMER_NAME}-${REMOTE_NODE_NAME}' using ${REMOTE_NODE_CF_FILE}"
+aws cloudformation deploy --stack-name "sopsCustomer-${CUSTOMER_NAME}-${REMOTE_NODE_NAME}" \
                                   --capabilities CAPABILITY_NAMED_IAM \
                                   --template-file ${CF_DIR}/${REMOTE_NODE_CF_FILE} \
-                                  --parameter-overrides ParameterKey=RemoteNodeName,ParameterValue=${REMOTE_NODE_NAME} ParameterKey=CustomerName,ParameterValue=${CUSTOMER_NAME} ParameterKey=CustomerS3BucketName,ParameterValue=${CUSTOMER_LOWER}
-fi
+                                  --parameter-overrides RemoteNodeName=${REMOTE_NODE_NAME} CustomerName=${CUSTOMER_NAME} CustomerS3BucketName=${CUSTOMER_LOWER}
+echo "--------------------"
+
--- scripts/deleteAWSCustomerStack.sh
+++ scripts/deleteAWSCustomerStack.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -e
 set -u
 
 ## LOAD required customer related vars
+SCRIPT=$( basename ${BASH_SOURCE[0]} )
 SCRIPT_DIR=$( dirname ${BASH_SOURCE[0]} )
-SCRIPT_DIR=$( readlink -f ${SCRIPT_DIR} )
 CF_DIR="${SCRIPT_DIR}/../CloudFormation/"
 . ${SCRIPT_DIR}/../vars.sh
 
-
+aws cloudformation delete-stack --stack-name "sopsCustomer-${CUSTOMER_NAME}-sqs-command-response"
 aws cloudformation delete-stack --stack-name "${CUSTOMER_NAME}Stack"
